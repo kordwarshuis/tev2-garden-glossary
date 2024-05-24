efd42b8698f29856c233d7c5b4feb72af45e59d0
kor <kor@dwarshuis.com>
2024-05-24 19:02:37 +0200

    feat: Add git reset --hard

--- reset.sh
+++ reset.sh
@@ -5,6 +5,8 @@ git checkout main
 
 # Remove the output branch
 git branch -d output
+# Back to the main branch original starting pointå
+git reset --hard
 
 # Remove everything untracked
 git clean -fd
