1414e2216f7e17f5dadbf533793f1e110405eec5
kor <kor@dwarshuis.com>
2024-05-24 18:16:35 +0200

    fix: Go to root of project

--- create_site.sh
+++ create_site.sh
@@ -4,5 +4,6 @@
 
 # Create a website based on the output of the TEv2 tools. In this case a MkDocs site.
 
-cd /
+# This command works by using git rev-parse --show-toplevel, which returns the root directory of the current Git repository. The $(...) syntax in bash executes the command inside the parentheses and replaces the $(...) with the command's output. So, cd $(git rev-parse --show-toplevel) changes the current directory to the root directory of the Git repository.
+cd $(git rev-parse --show-toplevel)
 mkdocs build
