32d304d6db8c5ba4a0f17072da2d72c87ebcb15e
kor <kor@dwarshuis.com>
2024-05-25 12:05:17 +0200

    refactor: Delete file

--- create_site.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Based on info found here: https://github.com/tno-terminology-design/tev2-mve/blob/main/.github/workflows/deploy-docs.yml
-
-# Create a website based on the output of the TEv2 tools. In this case a MkDocs site.
-
-# This command works by using git rev-parse --show-toplevel, which returns the root directory of the current Git repository. The $(...) syntax in bash executes the command inside the parentheses and replaces the $(...) with the command's output. So, cd $(git rev-parse --show-toplevel) changes the current directory to the root directory of the Git repository.
-cd $(git rev-parse --show-toplevel)
-mkdocs build
